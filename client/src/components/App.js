ab748a29fba299258f6385f624bfb8ef3f3892dc
Praveen Kumar <praveenscience@gmail.com>
2021-01-31 17:44:30 +0000

    Added the dark header to our App.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import Header from "./Header";
 
 class App extends Component {
   render() {
-    return <div className="App">Learn React</div>;
+    return (
+      <div className="App">
+        <Header dark={true} className="Header">
+          Leadstagram
+        </Header>
+      </div>
+    );
   }
 }
 
