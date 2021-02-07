4a1b0a8228300e3e47f596f1103b3d97677199ca
Praveen Kumar <praveenscience@gmail.com>
2021-02-07 14:59:10 +0000

    Displayed user data in a card.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import Users from "../constants/Users";
+import Card from "./Card";
 
 class App extends Component {
   state = {
@@ -15,6 +16,20 @@ class App extends Component {
         <Header dark={true} className="Header">
           Leadstagram
         </Header>
+        <div className="container-fluid">
+          <div className="row">
+            {this.state.Users.map((user, key) => (
+              <div className="col-12 col-md-6 col-lg-4 col-xl-3 mb-3">
+                <Card
+                  Image={"https://i.imgur.com/" + user.Imgur + "b.jpg"}
+                  ImgAlign="top"
+                  key={key}
+                  Header={user.Name}
+                ></Card>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     );
   }
