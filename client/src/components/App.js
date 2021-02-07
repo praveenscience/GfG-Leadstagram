2cd5693413023f249422090e96b4923d4094258e
Praveen Kumar <praveenscience@gmail.com>
2021-02-07 15:03:13 +0000

    Created a helper function to display social icons.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,6 +11,53 @@ class App extends Component {
     this.setState({ Users });
   }
   render() {
+    const SocialIcons = {
+      Facebook: {
+        IconClass: "social-facebook",
+        UrlPrepend: "https://www.facebook.com/"
+      },
+      Twitter: {
+        IconClass: "social-twitter",
+        UrlPrepend: "https://twitter.com/"
+      },
+      LinkedIn: {
+        IconClass: "social-linkedin",
+        UrlPrepend: "https://www.linkedin.com/in/"
+      },
+      Instagram: {
+        IconClass: "social-instagram",
+        UrlPrepend: "https://www.instagram.com/"
+      },
+      Quora: {
+        IconClass: "question",
+        UrlPrepend: "https://www.quora.com/"
+      },
+      YouTube: {
+        IconClass: "social-youtube",
+        UrlPrepend: "https://www.youtube.com/"
+      },
+      Reddit: {
+        IconClass: "social-reddit",
+        UrlPrepend: "https://www.reddit.com/"
+      },
+      Website: {
+        IconClass: "globe",
+        UrlPrepend: ""
+      },
+      Phone: {
+        IconClass: "screen-smartphone",
+        UrlPrepend: "tel:"
+      }
+    };
+    const Icon = ({ Network, Profile }) => (
+      <a
+        href={SocialIcons[Network].UrlPrepend + Profile}
+        className="btn btn-outline-success mr-2 btn-sm"
+      >
+        <i className={"icon-" + SocialIcons[Network].IconClass}></i>
+        {Network === "Phone" ? " " + Profile : null}
+      </a>
+    );
     return (
       <div className="App">
         <Header dark={true} className="Header">
