19300cc582b21faa678a72bcf5ebe148e366febf
Praveen Kumar <praveenscience@gmail.com>
2021-02-13 16:53:54 +0000

    Hitting the GET gives the list.

--- server/routes/api/todos.js
+++ server/routes/api/todos.js
@@ -1,8 +1,47 @@
 const express = require("express");
 const app = express.Router();
 
+const todos = [
+  "Milk",
+  "Yogurt",
+  "KurKure",
+  "Curd",
+  "Bread",
+  "Eggs",
+  "Banana",
+  "Oranges",
+  "Oats",
+  "Maple Syrup",
+  "Pancakes",
+  "Juice",
+  "Chicken",
+  "Paprika Powder",
+  "Cheese",
+  "Pasta",
+  "Bourbon",
+  "Aubergine",
+  "Maggi",
+  "Tomato",
+  "Red Chilies",
+  "Onions",
+  "Apple",
+  "Chips",
+  "Jalapeno",
+  "Paneer",
+  "Dosa",
+  "Bread",
+  "Rice",
+  "Chocolates",
+  "Croissant",
+  "Herbs and Spices",
+  "Sugar",
+  "Vinegar",
+  "Stock Cubes",
+  "Salt"
+];
+
 app.get("/", (req, res) => {
-  res.json("You're in /api/todos!");
+  res.json(todos);
 });
 
 module.exports = app;
--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const app = express.Router();
 
+const users = {
+  Praveen: {
+    Name: "Praveen Kumar",
+    WhatsApp: "+447405708485",
+    Email: "praveen@praveen.science",
+    Imgur: "T7bJCfJ",
+    Social: {
+      Facebook: "praveenscience",
+      Twitter: "praveenscience",
+      LinkedIn: "praveentech",
+      Instagram: "praveenscience",
+      Quora: null,
+      YouTube: "praveenscience",
+      Reddit: "praveenscience",
+      Website: "https://praveen.science"
+    }
+  }
+};
+
 app.get("/", (req, res) => {
-  res.json("You're in /api/users!");
+  res.json(users);
 });
 
 module.exports = app;
