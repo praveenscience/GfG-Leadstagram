a76423d5d999f3e3e5c84d936d6ee58b9b058772
Praveen Kumar <praveenscience@gmail.com>
2021-02-13 16:22:02 +0000

    Displayed Params, Query, Body of Requests.

--- server/server.js
+++ server/server.js
@@ -10,6 +10,19 @@ app.get("/", (req, res) => {
   res.json("Welcome to API Server!");
 });
 
+app.get("/:param", (req, res) => {
+  res.json(`Welcome to API Server, ${req.params.param}!`);
+});
+
+app.post(["/", "/:Name", "/:Profile/:Username"], express.json(), (req, res) => {
+  res.json({
+    Message: "I am in the post!",
+    Params: req.params,
+    Query: req.query,
+    Body: req.body
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server started in port ${port}.`);
 });
