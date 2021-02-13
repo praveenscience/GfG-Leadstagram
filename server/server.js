6285e170a4d3d60a7c1869be22c3cbb630da283d
Praveen Kumar <praveenscience@gmail.com>
2021-02-13 16:31:06 +0000

    Removed all routes.

--- server/server.js
+++ server/server.js
@@ -11,19 +11,6 @@ app.get("/", (req, res) => {
   res.json("Welcome to API Server!");
 });
 
-app.get("/:param", (req, res) => {
-  res.json(`Welcome to API Server, ${req.params.param}!`);
-});
-
-app.post(["/", "/:Name", "/:Profile/:Username"], (req, res) => {
-  res.json({
-    Message: "I am in the post!",
-    Params: req.params,
-    Query: req.query,
-    Body: req.body
-  });
-});
-
 app.listen(port, () => {
   console.log(`Server started in port ${port}.`);
 });
