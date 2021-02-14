80db8a0769158695067e0ca1ddacb758200902b0
Praveen Kumar <praveenscience@gmail.com>
2021-02-14 15:02:08 +0000

    Implemented adding todo with checks.

--- server/routes/api/todos.js
+++ server/routes/api/todos.js
@@ -3,15 +3,29 @@ const app = express.Router();
 
 const todos = require("../../constants/todos.json");
 
+// Get the complete list.
 app.get("/", (req, res) => {
   res.json(todos);
 });
-
+// Get the individual item.
 app.get("/:id", (req, res) => {
   if (todos[req.params.id]) {
     res.json(todos[req.params.id]);
   } else {
     res.status(404).json("Get lost!");
+  }
+});
+// Create a new item.
+app.post("/", (req, res) => {
+  if (typeof req.body.item !== "undefined") {
+    if (todos.includes(req.body.item)) {
+      res.status(409).json("How many times will you add?");
+    } else {
+      todos.push(req.body.item);
+      res.status(201).json("Thanks for adding " + req.body.item);
+    }
+  } else {
+    res.status(406).json("Give me item!");
   }
 });
 
