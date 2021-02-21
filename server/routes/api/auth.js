629e41d8749b6aed31a021df2ba868e5c9cfe737
Praveen Kumar <praveenscience@gmail.com>
2021-02-21 16:33:22 +0000

    Implemented validation for user.

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -6,11 +6,29 @@ const users = [
     Username: "Praveen",
     Password: "Praveen@123",
     FullName: "Praveen Kumar"
+  },
+  {
+    Username: "Abhishek",
+    Password: "Abhishek@123",
+    FullName: "Abhishek Kumar"
   }
 ];
 
 // Create new user
 // Validate existing user
+app.post("/login", (req, res) => {
+  const { Username, Password } = req.body;
+  let match = users.find(
+    u => u.Username === Username && u.Password === Password
+  );
+  if (match) {
+    // Make a shallow copy so that even if we remove password, it doesn't affect the original variable.
+    match = { ...match };
+    delete match.Password;
+    req.session.User = match;
+    res.status(`Successfully logged in as ${match.FullName}!`);
+  }
+});
 // Update existing user
 // Delete existing user
 
