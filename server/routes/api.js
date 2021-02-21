ff2957515525e535ad6cd47135fec0a8f236ec39
Praveen Kumar <praveenscience@gmail.com>
2021-02-21 16:16:00 +0000

    Auth skeleton added.

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express.Router();
+const auth = require("./api/auth");
 const users = require("./api/users");
 const todos = require("./api/todos");
 
 app.get("/", (req, res) => {
   res.json("You're in /api!");
 });
+app.use("/auth", auth);
 app.use("/users", users);
 app.use("/todos", todos);
 
---
+++ server/routes/api/auth.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express.Router();
+
+const users = [
+  {
+    Username: "Praveen",
+    Password: "Praveen@123",
+    FullName: "Praveen Kumar"
+  }
+];
+
+// Create new user
+// Validate existing user
+// Update existing user
+// Delete existing user
+
+module.exports = app;
