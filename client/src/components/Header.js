b835c1ba2031dd690a22c838e874636076b6f452
Praveen Kumar <praveenscience@gmail.com>
2021-02-21 16:44:33 +0000

    Upgraded headers to contain links.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,27 @@
 import React from "react";
 
-const Header = ({ dark, children, className }) => {
+const Header = ({ dark, children, className, items }) => {
   dark = !!dark ? "dark" : "light";
   return (
     <nav
       className={
-        `navbar navbar-${dark} bg-${dark}` + (className ? " " + className : "")
+        `Header navbar navbar-${dark} bg-${dark}` +
+        (className ? " " + className : "") +
+        (items && items.length ? " navbar-expand-lg" : "")
       }
     >
       <span className="navbar-brand">{children}</span>
+      {items && items.length > 0 && (
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav ml-auto">
+            {items.map((item, key) => (
+              <li className="nav-item" key={key}>
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
