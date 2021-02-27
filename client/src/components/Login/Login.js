d57a57062338d859f4fb775aab99b31c0991f23b
Praveen Kumar <praveenscience@gmail.com>
2021-02-27 15:53:11 +0000

    Used a helper to compress Login & Register.

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,18 +1,8 @@
 import React from "react";
-import Card from "../Bootstrap/Card";
+import GuestCardHelper from "./_Card";
 
 const Login = () => {
-  return (
-    <div className="Login">
-      <div className="container">
-        <div className="row">
-          <div className="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2 col-xl-6 offset-xl-3">
-            <Card Header="Sign In"></Card>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <GuestCardHelper Title="Login"></GuestCardHelper>;
 };
 
 export default Login;
--- client/src/components/Login/Register.js
+++ client/src/components/Login/Register.js
@@ -1,18 +1,8 @@
 import React from "react";
-import Card from "../Bootstrap/Card";
+import GuestCardHelper from "./_Card";
 
 const Register = () => {
-  return (
-    <div className="Register">
-      <div className="container">
-        <div className="row">
-          <div className="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2 col-xl-6 offset-xl-3">
-            <Card Header="Register"></Card>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <GuestCardHelper Title="Register"></GuestCardHelper>;
 };
 
 export default Register;
