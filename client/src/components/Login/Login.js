4fc782b2bc36a4339414fef6aa500b796cf04c16
Praveen Kumar <praveenscience@gmail.com>
2021-02-27 15:48:15 +0000

    Login & Register Pages done with Header.

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Card from "../Bootstrap/Card";
 
 const Login = () => {
-  return <div className="Login">Login Page</div>;
+  return (
+    <div className="Login">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2 col-xl-6 offset-xl-3">
+            <Card Header="Sign In"></Card>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- client/src/components/Login/Register.js
+++ client/src/components/Login/Register.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Card from "../Bootstrap/Card";
 
 const Register = () => {
-  return <div className="Register">Register Page</div>;
+  return (
+    <div className="Register">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2 col-xl-6 offset-xl-3">
+            <Card Header="Register"></Card>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
