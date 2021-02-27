38b85950af9c32cda381f99559188b35f34ab1d2
Praveen Kumar <praveenscience@gmail.com>
2021-02-27 16:23:28 +0000

    Created a helper for all the form elements.

---
+++ client/src/components/Login/FormElements.js
@@ -0,0 +1,26 @@
+export default {
+  Email: {
+    Id: "Email",
+    Label: "Email",
+    Type: "email",
+    Desc: "Please enter your email address."
+  },
+  Password: {
+    Id: "Password",
+    Label: "Password",
+    Type: "password",
+    Desc: "Please enter a memorable password."
+  },
+  ConfPass: {
+    Id: "ConfPass",
+    Label: "Confirm Password",
+    Type: "password",
+    Desc: "Please re-enter the same password."
+  },
+  FullName: {
+    Id: "FullName",
+    Label: "Full Name",
+    Type: "text",
+    Desc: "Please enter your full name here."
+  }
+};
