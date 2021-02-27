91cb8bcc22825e55d370f27f9f0b3fb89014c87b
Praveen Kumar <praveenscience@gmail.com>
2021-02-27 16:23:39 +0000

    Included different form elements.

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,8 +1,11 @@
 import React from "react";
 import GuestCardHelper from "./_Card";
+import fe from "./FormElements";
 
 const Login = () => {
-  return <GuestCardHelper Title="Login" FormElements={[]} />;
+  return (
+    <GuestCardHelper Title="Login" FormElements={[fe.Email, fe.Password]} />
+  );
 };
 
 export default Login;
--- client/src/components/Login/Register.js
+++ client/src/components/Login/Register.js
@@ -1,8 +1,14 @@
 import React from "react";
 import GuestCardHelper from "./_Card";
+import fe from "./FormElements";
 
 const Register = () => {
-  return <GuestCardHelper Title="Register" FormElements={[]} />;
+  return (
+    <GuestCardHelper
+      Title="Register"
+      FormElements={[fe.Email, fe.Password, fe.ConfPass, fe.FullName]}
+    />
+  );
 };
 
 export default Register;
