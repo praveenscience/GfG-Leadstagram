d67188e48f859cb0c42f0ce502db27836ac212e1
Praveen Kumar <praveenscience@gmail.com>
2021-02-27 15:29:19 +0000

    Setup routes for Guest.

--- client/src/components/Pages/Guest.js
+++ client/src/components/Pages/Guest.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import Welcome from "../Login/Welcome";
 import Login from "../Login/Login";
 import Register from "../Login/Register";
@@ -6,9 +7,9 @@ import Register from "../Login/Register";
 const Guest = () => {
   return (
     <div className="Guest">
-      <Welcome />
-      <Login />
-      <Register />
+      <Route path="/" exact={true} component={Welcome} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
     </div>
   );
 };
