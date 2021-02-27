0c31a73066f77ad84f8a77066d0cc9828dca2d1a
Praveen Kumar <praveenscience@gmail.com>
2021-02-27 16:23:14 +0000

    Added Child Support.

--- client/src/components/Login/_Card.js
+++ client/src/components/Login/_Card.js
@@ -2,13 +2,14 @@ import React from "react";
 import Card from "../Bootstrap/Card";
 import FormGroup from "../Bootstrap/FormGroup";
 
-const GuestCardHelper = ({ Title, FormElements }) => {
+const GuestCardHelper = ({ Title, FormElements, children }) => {
   return (
     <div className={Title}>
       <div className="container">
         <div className="row">
           <div className="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2 col-xl-6 offset-xl-3">
             <Card Header={Title}>
+              {children}
               <form>
                 {FormElements &&
                   FormElements.map((fg, key) => (
