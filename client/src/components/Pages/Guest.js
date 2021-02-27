448899de9d50ac9a383e99fbd751b4994ba12e4c
Praveen Kumar <praveenscience@gmail.com>
2021-02-27 16:31:43 +0000

    Converted Guest into Class Component.

--- client/src/components/Pages/Guest.js
+++ client/src/components/Pages/Guest.js
@@ -1,24 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import Welcome from "../Login/Welcome";
 import Login from "../Login/Login";
 import Register from "../Login/Register";
 
-const Guest = () => {
-  return (
-    <div className="Guest">
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/" exact={true} component={Welcome} />
-        <Route>
-          <div className="NotFound">
-            <p>Not Found</p>
-          </div>
-        </Route>
-      </Switch>
-    </div>
-  );
-};
+class Guest extends Component {
+  render() {
+    return (
+      <div className="Guest">
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/" exact={true} component={Welcome} />
+          <Route>
+            <div className="NotFound">
+              <p>Not Found</p>
+            </div>
+          </Route>
+        </Switch>
+      </div>
+    );
+  }
+}
 
 export default Guest;
