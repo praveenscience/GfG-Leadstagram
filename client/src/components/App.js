6c6b6dc902241569ce7949e4ff6c3dcb02592376
Praveen Kumar <praveenscience@gmail.com>
2021-03-06 15:17:34 +0000

    Displayed a loading message before AJAX calls.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,8 @@ import Home from "./Pages/Home";
 
 class App extends Component {
   state = {
-    LoggedIn: false
+    LoggedIn: false,
+    AJAXCall: false
   };
   setLoggedIn = LoggedIn => {
     this.setState({ LoggedIn });
@@ -19,55 +20,75 @@ class App extends Component {
     });
   };
   componentDidMount() {
-    GetCurrentUser().then(res => {
-      if (res.status === 200) {
-        this.setState({
-          LoggedIn: res.data.FullName
-        });
-      }
-    });
+    GetCurrentUser()
+      .then(res => {
+        if (res.status === 200) {
+          this.setState({
+            LoggedIn: res.data.FullName
+          });
+        }
+      })
+      .catch(() => {})
+      .then(() => {
+        this.setState({ AJAXCall: true });
+      });
   }
   render() {
     return (
       <div className="App">
-        <Header
-          dark={true}
-          className="Header"
-          to="/"
-          Link={Link}
-          items={
-            this.state.LoggedIn
-              ? [
-                  <span className="d-inline-block pr-3 pt-1 text-white user-info">
-                    Welcome {this.state.LoggedIn}
-                  </span>,
-                  <Link
-                    to="/logout"
-                    className="btn btn-danger btn-sm"
-                    onClick={this.handleLogout}
-                  >
-                    Sign Out
-                  </Link>
-                ]
-              : [
-                  <Link to="/" className="btn btn-primary btn-sm ml-3">
-                    Welcome
-                  </Link>,
-                  <Link to="/login" className="btn btn-primary btn-sm ml-3">
-                    Sign In
-                  </Link>,
-                  <Link to="/register" className="btn btn-primary btn-sm ml-3">
-                    Register
-                  </Link>
-                ]
-          }
-        >
-          Leadstagram
-        </Header>
-        {this.state.LoggedIn ? (
-          <Home />
+        {this.state.AJAXCall ? (
+          <>
+            <Header
+              dark={true}
+              className="Header"
+              to="/"
+              Link={Link}
+              items={
+                this.state.LoggedIn
+                  ? [
+                      <span className="d-inline-block pr-3 pt-1 text-white user-info">
+                        Welcome {this.state.LoggedIn}
+                      </span>,
+                      <Link
+                        to="/logout"
+                        className="btn btn-danger btn-sm"
+                        onClick={this.handleLogout}
+                      >
+                        Sign Out
+                      </Link>
+                    ]
+                  : [
+                      <Link to="/" className="btn btn-primary btn-sm ml-3">
+                        Welcome
+                      </Link>,
+                      <Link to="/login" className="btn btn-primary btn-sm ml-3">
+                        Sign In
+                      </Link>,
+                      <Link
+                        to="/register"
+                        className="btn btn-primary btn-sm ml-3"
+                      >
+                        Register
+                      </Link>
+                    ]
+              }
+            >
+              Leadstagram
+            </Header>
+            {this.state.LoggedIn ? (
+              <Home />
+            ) : (
+              <Guest setLoggedIn={this.setLoggedIn} />
+            )}
+          </>
         ) : (
-          <Guest setLoggedIn={this.setLoggedIn} />
+          <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <div className="alert alert-warning mt-3">Loading...</div>
+              </div>
+            </div>
+          </div>
         )}
       </div>
     );
